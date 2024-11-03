8fb4a21d75f7fd82e1a039516539a83e2626eb24
Niall Young <5465765+niallyoung@users.noreply.github.com>
2024-11-03 13:58:00 +1100

    initial tomtom implementation, TODO WIP: mocked axios response data

--- src/tomtom.ts
+++ src/tomtom.ts
@@ -1,4 +1,29 @@
-export async function addressSearch(address: any): Promise<any> {
-    console.log("tomtom.addressSearch() invoked")
-    return Promise.resolve(address);
+import { SearchResult } from './index'
+
+const axios = require('axios').default;
+const tomTomURL = "https://api.tomtom.com";
+
+export interface addressSearchParams {
+    key: string;
+    address: any;
+    limit?: number;
+    countrySet?: string;
+}
+
+export async function addressSearch(searchParams: addressSearchParams) {
+    const response = await axios.get(tomTomURL + `/search/2/search/${searchParams.address}.json'`, {
+        params: { ...searchParams }
+    });
+
+    return response.data.results.map((result) => {
+        const searchResult: SearchResult = {
+            placeId: result.id,
+            streetNumber: result.streetNumber,
+            countryCode: result.countryCode,
+            country: result.country,
+            freeformAddress: result.freeformAddress,
+            municipality: result.municipality
+        }
+        return searchResult;
+    })
 }
--- test/tomtom.test.ts
+++ test/tomtom.test.ts
@@ -1,14 +1,55 @@
+import { SearchResult } from '../src'
 import { config } from 'dotenv'
 import { addressSearch} from '../src/tomtom';
-import { describe, it, expect } from '@jest/globals'
+import { describe, expect, it } from '@jest/globals'
 
 config()
+const apiKey = process.env.TOMTOM_API_KEY;
 
-describe('tomtom', () => {
-    describe('addressSearch returns a promise', () => {
-        it('returns a promise', () => {
-            const results = addressSearch('Charlotte Street')
-            expect(results).toBeInstanceOf(Promise)
-        })
+const fakeResults: SearchResult[] = [
+    {
+        placeId: "fake-place-id-1",
+        streetNumber: "fake-street-number-1",
+        countryCode: "fake-country-code-1",
+        country: "fake-country-1",
+        freeformAddress: "fake-free-form-address-1",
+        municipality: "fake-municipality-1"
+    },
+    {
+        placeId: "fake-place-id-2",
+        streetNumber: "fake-street-number-2",
+        countryCode: "fake-country-code-2",
+        country: "fake-country-2",
+        freeformAddress: "fake-free-form-address-2",
+        municipality: "fake-municipality-2"
+    }
+]
+
+describe('addressSearch()', () => {
+    it('returns a promise', () => {
+        const searchResults = addressSearch({key: apiKey, address: 'Charlotte Street'})
+        expect(searchResults).toBeInstanceOf(Promise)
     })
 })
+
+// describe('with mocked axios data, addressSearch()', () => {
+//     const mockAxios = axios as jest.Mocked<typeof axios>;
+//     beforeEach(() => {
+//         mockAxios.request.mockImplementationOnce((): Promise<any> => Promise.resolve({
+//             data: { results: [fakeResults] }
+//         }));
+//     });
+//
+//     it('returns the expected results', async () => {
+//         const searchResults = await addressSearch({key: apiKey, address: 'Smith St'})
+//         expect(axios.get).toHaveBeenCalledTimes(1);
+//         expect(searchResults).resolves.toContain({
+//             placeId: "fake-place-id-1",
+//             streetNumber: "fake-street-number-1",
+//             countryCode: "fake-country-code-1",
+//             country: "fake-country-1",
+//             freeformAddress: "fake-free-form-address-1",
+//             municipality: "fake-municipality-1"
+//         })
+//     })
+// })
